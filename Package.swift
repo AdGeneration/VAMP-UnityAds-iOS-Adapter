ddd0eab07335e09477ecec87402e214ae9a81fe9
AdGeneration <ssp-ope@supership.jp>
2026-09-15 16:45:00 +0900

    4.20.100

--- Package.swift
+++ Package.swift
@@ -36,13 +36,13 @@ let package = Package(
         
         .binaryTarget(
             name: "VAMPUnityAdsAdapter",
-            url: "https://github.com/AdGeneration/VAMP-UnityAds-iOS-Adapter/releases/download/4.20.0/VAMPUnityAdsAdapter-v4.20.0.zip",
-            checksum: "ce23352807c8a3efb690a4feeac9f04feddd5d7134dc8656485db8e2d6095a61"
+            url: "https://github.com/AdGeneration/VAMP-UnityAds-iOS-Adapter/releases/download/4.20.100/VAMPUnityAdsAdapter-v4.20.100.zip",
+            checksum: "a2e792debcccc62a07fa32c305d7c1e927dce1b2da11b605fe26b61f480b949a"
         ),
         .binaryTarget(
             name: "UnityAds",
-            url: "https://github.com/Unity-Technologies/unity-ads-ios/releases/download/4.20.0/UnityAds.zip",
-            checksum: "7084559dd2d2ed4c6bf2244917fee557db67e21fef66796c7e42da2bae6685d4"
+            url: "https://github.com/Unity-Technologies/unity-ads-ios/releases/download/4.20.1/UnityAds.zip",
+            checksum: "c7fba62bec9fe1f703caf83931cce65452bbfca4a3e742c338d57d845c4ad840"
         )
     ]
 )
